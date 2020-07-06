3f6f2a1f2211d1d47b5a973cc4e2d8c3afa01571
Mike Wallbank <wallbank@fnal.gov>
2020-07-06 16:38:50 -0500

    simple utility script to cat all the output files together into a single file

---
+++ merge_output.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# merge_output.sh
+# Mike Wallbank
+#
+# Usage: merge_output.sh /path/to/files/ [option]
+# When no option is specified, the related files in each subdirectory are merged
+# When option 'all' is specified, the merged files in each subdirectory are merged into the final output in the specific directory
+
+if [ "$#" -ne 1 ] && [ "$#" -ne 2 ]; then
+    echo "Useage: merge_output.sh /path/to/files/ [option]"
+    exit
+fi
+
+if [ "$2" = "all" ]; then
+    for det in $(seq 20 26); do
+	echo "Merging detector $det files into $1/${det}_all.txt"
+	touch $1/${det}_all.txt
+	chmod +w $1/${det}_all.txt
+	cat $1/*/${det}_all.txt > $1/${det}_all.txt
+    done
+else
+    for dir in `ls $1`; do
+	for det in $(seq 20 26); do
+	    echo "Merging detector $det files into $1/${dir}/${det}_all.txt"
+	    touch $1/${dir}/${det}_all.txt
+	    chmod +w $1/${dir}/${det}_all.txt
+	    cat $1/${dir}/${det}_*.txt > $1/${dir}/${det}_all.txt
+	done
+    done
+fi
