4ac2146012cb56fa9d748b6308112316196c6da4
Mike Wallbank <wallbank@fnal.gov>
2020-07-06 16:36:46 -0500

    separate out the job submitting into batches so as to not overwhelm the dCache file system\!

--- jobsub.sh
+++ jobsub.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+JOBS_PER_BATCH=1000
+EVENTS_PER_JOB=400000
+
 if [ "$#" == 1 ] && [[ "$1" = "nova" ]]; then
-    jobsub_submit --resource-provides=usage_model=OPPORTUNISTIC --OS=SL6 -G nova -N 10000 -f dropbox://NOvA_TB.in -d OUTDIR /pnfs/nova/scratch/users/wallbank/testbeam_sim/ file://run_g4bl_nova.sh 400000 OUTDIR
+    for BATCH in {0,3,4}
+    do
+	BATCH_PROCESS_START=$((JOBS_PER_BATCH*BATCH))
+	BATCH_EVENT_START=$((EVENTS_PER_JOB*BATCH_PROCESS_START))
+	jobsub_submit --resource-provides=usage_model=OPPORTUNISTIC --OS=SL6 -G nova -N ${JOBS_PER_BATCH} -f dropbox://NOvA_TB.in -f dropbox://EPB.in -f dropbox://QQM.in -f dropbox://RQB.in -f dropbox://EPB_fieldmap.txt -f dropbox://3Q120_fieldmap.txt -f dropbox://4Q120_fieldmap.txt -d OUTDIR /pnfs/nova/scratch/users/wallbank/testbeam_sim/full_fields/neg64GeV/${BATCH_EVENT_START} file://run_g4bl_nova.sh ${BATCH_PROCESS_START} ${EVENTS_PER_JOB} OUTDIR
+    done
 
 else
     jobsub_submit  --resource-provides=usage_model=OPPORTUNISTIC --expected-lifetime=2h --disk=4GB -G ebd -N 50 file://run_g4bl.sh
