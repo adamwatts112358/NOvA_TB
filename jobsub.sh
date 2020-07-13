8930dc05d356b5b4eea47a2ade6866a1716e6406
Mike Wallbank <wallbank@fnal.gov>
2020-07-13 11:31:24 -0500

    configuration to run full simulation with all virtual detectors

--- jobsub.sh
+++ jobsub.sh
@@ -2,13 +2,20 @@
 
 JOBS_PER_BATCH=1000
 EVENTS_PER_JOB=400000
+OUTPUT_DIR='/pnfs/nova/persistent/users/wallbank/testbeam/plume_sim/full_fields/64GeV/mc6mc7/'
 
 if [ "$#" == 1 ] && [[ "$1" = "nova" ]]; then
-    for BATCH in {0,3,4}
+    for BATCH in {0..4}
     do
 	BATCH_PROCESS_START=$((JOBS_PER_BATCH*BATCH))
 	BATCH_EVENT_START=$((EVENTS_PER_JOB*BATCH_PROCESS_START))
-	jobsub_submit --resource-provides=usage_model=OPPORTUNISTIC --OS=SL6 -G nova -N ${JOBS_PER_BATCH} -f dropbox://NOvA_TB.in -f dropbox://EPB.in -f dropbox://QQM.in -f dropbox://RQB.in -f dropbox://EPB_fieldmap.txt -f dropbox://3Q120_fieldmap.txt -f dropbox://4Q120_fieldmap.txt -d OUTDIR /pnfs/nova/scratch/users/wallbank/testbeam_sim/full_fields/neg64GeV/${BATCH_EVENT_START} file://run_g4bl_nova.sh ${BATCH_PROCESS_START} ${EVENTS_PER_JOB} OUTDIR
+
+	touch ${OUTPUT_DIR}/${BATCH_EVENT_START}
+	rm -rf ${OUTPUT_DIR}/${BATCH_EVENT_START}
+	mkdir ${OUTPUT_DIR}/${BATCH_EVENT_START}
+	chmod g+w ${OUTPUT_DIR}/${BATCH_EVENT_START}
+
+	jobsub_submit --resource-provides=usage_model=OPPORTUNISTIC --OS=SL6 -G nova -N ${JOBS_PER_BATCH} -f dropbox://NOvA_TB.in -f dropbox://EPB.in -f dropbox://QQM.in -f dropbox://RQB.in -f dropbox://EPB_fieldmap.txt -f dropbox://3Q120_fieldmap.txt -f dropbox://4Q120_fieldmap.txt -d OUTDIR ${OUTPUT_DIR}/${BATCH_EVENT_START} file://run_g4bl_nova.sh ${BATCH_PROCESS_START} ${EVENTS_PER_JOB} OUTDIR
     done
 
 else
